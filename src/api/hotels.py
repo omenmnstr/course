89a86a0a897064b7886c613a63e20ed573be49a4
ivo <sw1zyyy.31@gmail.com>
2025-09-28 00:21:27 +0300

    task 4

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,23 +1,17 @@
 from fastapi import Query, APIRouter, Body
-from sqlalchemy.ext.asyncio import async_sessionmaker
-from  sqlalchemy import insert, select, func
-from database import async_session_maker, engine
-from models.hotels import HotelsOrm
 from repositories.hotels import HotelsRepository
 from src.api.dependencies import PaginationDep
+from src.database import async_session_maker
+
 from src.schemas.hotels import Hotel, HotelPATCH
 
-
 router = APIRouter(prefix="/hotels", tags=["Отели"])
-
-
-
 
 
 @router.get("")
 async def get_hotels(
         pagination: PaginationDep,
-        location: str | None = Query(None, description="Город"),
+        location: str | None = Query(None, description="Локация"),
         title: str | None = Query(None, description="Название отеля"),
 ):
     per_page = pagination.per_page or 5
@@ -25,49 +19,49 @@ async def get_hotels(
         return await HotelsRepository(session).get_all(
             location=location,
             title=title,
-            limit = per_page,
-            offset=per_page * (pagination.page-1))
+            limit=per_page,
+            offset=per_page * (pagination.page - 1)
+        )
 
-    #per_page = pagination.per_page or 5
-    #async with async_session_maker() as session:
-       #query = select(HotelsOrm)
-        #if location:
-        #    query = query.filter(func.lower(HotelsOrm.location).contains(location.strip().lower()))
-       # if title:
-       #     query = query.filter(func.lower(HotelsOrm.title).contains(title.strip().lower()))
-        #query = (
-       #     query
-        #    .limit(per_page)
-       #     .offset (per_page * (pagination.page-1))
-       # )
-        #result = await session.execute(query)
-        #hotels = result.scalars().all()
-
-
-    # if pagination.page and pagination.per_page:
-        #return hotels_[pagination.per_page * (pagination.page-1):][:pagination.per_page]
-
+@router.get("/{hotel_id}")
+async def get_hotel(hotel_id: int):
+    async with async_session_maker() as session:
+        hotel = await HotelsRepository(session).get_one_or_none(id=hotel_id)
+        if not hotel:
+            return {"status": "error", "message": "Отель не найден"}
+        return hotel
 
 
 @router.post("")
 async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
-    "1": {},
-    "2": {}
+    "1": {
+        "summary": "Сочи",
+        "value": {
+            "title": "Отель Сочи 5 звезд у моря",
+            "location": "ул. Моря, 1",
+        }
+    },
+    "2": {
+        "summary": "Дубай",
+        "value": {
+            "title": "Отель Дубай У фонтана",
+            "location": "ул. Шейха, 2",
+        }
+    }
 })
 ):
     async with async_session_maker() as session:
-       hotel = await HotelsRepository(session).add(hotel_data)
-       await session.commit()
+        hotel = await HotelsRepository(session).add(hotel_data)
+        await session.commit()
 
-    return {"status": "OK"}
+    return {"status": "OK", "data": hotel}
 
 
 @router.put("/{hotel_id}")
-def edit_hotel(hotel_id: int, hotel_data: Hotel):
-    global hotels
-    hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
-    hotel["title"] = hotel_data.title
-    hotel["name"] = hotel_data.name
+async def edit_hotel(hotel_id: int, hotel_data: Hotel):
+    async with async_session_maker() as session:
+        await HotelsRepository(session).edit(hotel_data, id=hotel_id)
+        await session.commit()
     return {"status": "OK"}
 
 
@@ -76,21 +70,19 @@ def edit_hotel(hotel_id: int, hotel_data: Hotel):
     summary="Частичное обновление данных об отеле",
     description="<h1>Тут мы частично обновляем данные об отеле: можно отправить name, а можно title</h1>",
 )
-def partially_edit_hotel(
+async def partially_edit_hotel(
         hotel_id: int,
         hotel_data: HotelPATCH,
 ):
-    global hotels
-    hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
-    if hotel_data.title:
-        hotel["title"] = hotel_data.title
-    if hotel_data.name:
-        hotel["name"] = hotel_data.name
+    async with async_session_maker() as session:
+        await HotelsRepository(session).edit(hotel_data, exclude_unset=True, id=hotel_id)
+        await session.commit()
     return {"status": "OK"}
 
 
 @router.delete("/{hotel_id}")
-def delete_hotel(hotel_id: int):
-    global hotels
-    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
+async def delete_hotel(hotel_id: int):
+    async with async_session_maker() as session:
+        await HotelsRepository(session).delete(id=hotel_id)
+        await session.commit()
     return {"status": "OK"}
--- src/repositories/base.py
+++ src/repositories/base.py
@@ -1,42 +1,37 @@
-from pydantic import BaseModel
 from sqlalchemy import select, insert, update, delete
+from pydantic import BaseModel
 
 
-class BaseRepository():
+class BaseRepository:
     model = None
 
-    def __init__(self, session):
+def __init__(self, session):
         self.session = session
 
-    async def get_all(self, *args, **kwargs):
+async def get_all(self, *args, **kwargs):
         query = select(self.model)
         result = await self.session.execute(query)
-
         return result.scalars().all()
 
-    async def get_one_or_none(self, **filter_by):
+async def get_one_or_none(self, **filter_by):
         query = select(self.model).filter_by(**filter_by)
         result = await self.session.execute(query)
-
         return result.scalars().one_or_none()
 
-
-    async def add(self, data: BaseModel):
+async def add(self, data: BaseModel):
         add_data_stmt = insert(self.model).values(**data.model_dump()).returning(self.model)
         result = await self.session.execute(add_data_stmt)
         return result.scalars().one()
 
-    async def edit(self, data: BaseModel, **filter_by) -> None:
-        update_data = data.model_dump(exclude_unset=True)
-        if not update_data:
-            return
-        stmt = update(self.model).values(**update_data).filter_by(**filter_by)
-        await self.session.execute(stmt)
-        await self.session.commit()
 
+async def edit(self, data: BaseModel, exclude_unset: bool = False, **filter_by) -> None:
+        update_stmt = (
+            update(self.model)
+            .filter_by(**filter_by)
+            .values(**data.model_dump(exclude_unset=exclude_unset))
+        )
+        await self.session.execute(update_stmt)
 
-    async def delete(self, **filter_by) -> None:
-        stmt = delete(self.model).filter_by(**filter_by)
-        await self.session.execute(stmt)
-        await self.session.commit()
-
+async def delete(self, **filter_by) -> None:
+        delete_stmt = delete(self.model).filter_by(**filter_by)
+        await self.session.execute(delete_stmt)
